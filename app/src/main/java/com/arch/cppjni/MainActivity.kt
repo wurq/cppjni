b982bd13744fe67ea27ebd29b37e1ba3e6050dde
wurongqiu <wurongqiu@kaike.la>
2018-06-21 14:27:16 +0800

    Initial commit

---
+++ app/src/main/java/com/arch/cppjni/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.arch.cppjni
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Example of a call to a native method
+        sample_text.text = stringFromJNI()
+
+//        sample_text.setOnClickListener()
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    external fun stringFromJNI(): String
+
+    companion object {
+
+        // Used to load the 'native-lib' library on application startup.
+        init {
+            System.loadLibrary("native-lib")
+        }
+    }
+}
+
+private fun View.setOnClickListener() {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+}
